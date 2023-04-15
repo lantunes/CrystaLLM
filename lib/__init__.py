b74d1877dc4cb43d4636c63bf5bd7b58d5a8b154
Luis Antunes <lantunes@gmail.com>
2023-04-15 19:59:31 -0400

    adding structure-based metrics

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,7 @@
 from ._tokenizer import get_cif_tokenizer
 from ._data import CIFData, populate_cif_data
-from ._metrics import abs_r_score
+from ._metrics import abs_r_score, bond_length_reasonableness_score, is_space_group_consistent, \
+    is_atom_site_occupancy_consistent
 from ._preprocessing import array_split
 from ._utils import get_unit_cell_volume, plot_true_vs_predicted, get_composition_permutations, \
     get_oxi_state_decorated_structure, get_atomic_props_block
--- lib/_metrics.py
+++ lib/_metrics.py
@@ -1,5 +1,10 @@
 import numpy as np
+from pymatgen.analysis.local_env import CrystalNN
 import warnings
+
+from pymatgen.core import Composition, Structure
+from pymatgen.io.cif import CifParser
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 
 def abs_r_score(actual, predicted):
@@ -36,3 +41,100 @@ def abs_r_score(actual, predicted):
     sad = np.sum(np.abs(actual - np.mean(actual)))
 
     return 1 - (sae / sad)
+
+
+def bond_length_reasonableness_score(structure, tolerance=0.3):
+    """
+    If a bond length is 30% shorter or longer than the sum of the atomic radii, the score is lower.
+
+    :param structure:
+    :return:
+    """
+    crystal_nn = CrystalNN()
+
+    min_ratio = 1 - tolerance
+    max_ratio = 1 + tolerance
+
+    # calculate the score based on bond lengths and covalent radii
+    score = 0
+    bond_count = 0
+    for i, site in enumerate(structure):
+        bonded_sites = crystal_nn.get_nn_info(structure, i)
+        for connected_site_info in bonded_sites:
+            connected_site = connected_site_info['site']
+            bond_length = site.distance(connected_site)
+
+            electronegativity_diff = abs(site.specie.X - connected_site.specie.X)
+            """
+            According to the Pauling scale, when the electronegativity difference 
+            between two bonded atoms is less than 1.7, the bond can be considered 
+            to have predominantly covalent character, while a difference greater 
+            than or equal to 1.7 indicates that the bond has significant ionic 
+            character.
+            """
+            if electronegativity_diff >= 1.7:
+                # use ionic radii
+                if site.specie.X < connected_site.specie.X:
+                    expected_length = site.specie.average_cationic_radius + connected_site.specie.average_anionic_radius
+                else:
+                    expected_length = site.specie.average_anionic_radius + connected_site.specie.average_cationic_radius
+            else:
+                expected_length = site.specie.atomic_radius + connected_site.specie.atomic_radius
+
+            bond_ratio = bond_length / expected_length
+
+            # penalize bond lengths that are too short or too long
+            if min_ratio < bond_ratio < max_ratio:
+                score += 1
+
+            bond_count += 1
+
+    normalized_score = score / bond_count
+
+    return normalized_score
+
+
+def is_space_group_consistent(cif_str):
+    structure = Structure.from_str(cif_str, fmt="cif")
+    parser = CifParser.from_string(cif_str)
+    cif_data = parser.as_dict()
+
+    # Extract the stated space group from the CIF file
+    stated_space_group = cif_data[list(cif_data.keys())[0]]['_symmetry_space_group_name_H-M']
+
+    # Analyze the symmetry of the structure
+    spacegroup_analyzer = SpacegroupAnalyzer(structure, symprec=0.1)
+
+    # Get the detected space group
+    detected_space_group = spacegroup_analyzer.get_space_group_symbol()
+
+    # Check if the detected space group matches the stated space group
+    is_match = stated_space_group.strip() == detected_space_group.strip()
+
+    return is_match
+
+
+def is_atom_site_occupancy_consistent(cif_str):
+    # Parse the CIF string
+    parser = CifParser.from_string(cif_str)
+    cif_data = parser.as_dict()
+
+    # Extract the chemical formula sum from the CIF data
+    formula_sum = cif_data[list(cif_data.keys())[0]]["_chemical_formula_sum"]
+
+    # Convert the formula sum into a dictionary
+    expected_atoms = Composition(formula_sum).as_dict()
+
+    # Count the atoms provided in the _atom_site_type_symbol section
+    actual_atoms = {}
+    for key in cif_data:
+        if "_atom_site_type_symbol" in cif_data[key] and "_atom_site_symmetry_multiplicity" in cif_data[key]:
+            for atom_type, multiplicity in zip(cif_data[key]["_atom_site_type_symbol"],
+                                               cif_data[key]["_atom_site_symmetry_multiplicity"]):
+                if atom_type in actual_atoms:
+                    actual_atoms[atom_type] += int(multiplicity)
+                else:
+                    actual_atoms[atom_type] = int(multiplicity)
+
+    # Validate if the expected and actual atom counts match
+    return expected_atoms == actual_atoms
