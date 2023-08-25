5760bd6c06793292c215883344f747537dcf2a53
Luis Antunes <lantunes@gmail.com>
2023-08-25 02:16:42 -0400

    setting request timeout

--- lib/_zmq_scorer.py
+++ lib/_zmq_scorer.py
@@ -4,18 +4,28 @@ from lib import CIFScorer
 
 class ZMQScorer(CIFScorer):
 
-    def __init__(self, port: int):
+    def __init__(self, port: int, timeout_ms: int = 10000):
         self._port = port
         print(f"ZeroMQ CIFScorer using port: {port}")
 
         # prepare the ZeroMQ context and REQ socket
         context = zmq.Context()
         self._socket = context.socket(zmq.REQ)
+
+        # set the socket timeout
+        self._socket.setsockopt(zmq.RCVTIMEO, timeout_ms)
+        self._socket.setsockopt(zmq.SNDTIMEO, timeout_ms)
+
         self._socket.connect(f"tcp://localhost:{self._port}")
 
     def score(self, cif) -> float:
-        # send a request and wait for the reply
-        self._socket.send_string(cif)
-        message = self._socket.recv_string()
+        try:
+            # send a request
+            self._socket.send_string(cif)
 
-        return float(message)
+            # wait for the reply
+            message = self._socket.recv_string()
+            return float(message)
+
+        except zmq.Again as e:
+            raise TimeoutError("ZeroMQ request timed out") from e
